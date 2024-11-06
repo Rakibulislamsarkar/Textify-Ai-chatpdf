3ea24e58e559d8b65851d2ffd10f7faa2b72eb1b
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2024-11-07 00:51:55 +0530

    11702

--- src/app/dashboard/_components/UploadPdfDialogue.tsx
+++ src/app/dashboard/_components/UploadPdfDialogue.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from "react";
+'use client';
+
+import React, { ReactNode , FormEvent, useRef, useState } from "react";
 import {
   Dialog,
   DialogClose,
@@ -11,11 +13,39 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Loader2 } from "lucide-react";
 
 // Define props type for the component
 type Props = { children: ReactNode };
 
 const UploadPdf: React.FC<Props> = ({ children }) => {
+
+  const generateUploadUrl = useMutation(api.messages.generateUploadUrl);
+  const [file, setFile] = useState<File | null>(null);
+  const [loading , setLoading] = useState(false);
+
+  const OnFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+  const OnUpload = async () =>{
+    setLoading(true);
+    // Step 1: Get a short-lived upload URL
+    const postUrl = await generateUploadUrl();
+
+    // Step 2: POST the file to the URL
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: file?.type ? { "Content-Type": file.type } : undefined,
+      body: file,
+    });
+    const { storageId } = await result.json();
+  }
+  
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -30,7 +60,7 @@ const UploadPdf: React.FC<Props> = ({ children }) => {
               </div>
               <div className="mt-4">
                 <label className="block text-base font-medium mb-1 text-left">File Name</label>
-                <Input placeholder="Enter File Name" className="w-full" />
+                <Input placeholder="Enter File Name" className="w-full" onChange={(event) => OnFileSelect(event)} />
               </div>
             </div>
           </DialogDescription>
@@ -39,7 +69,10 @@ const UploadPdf: React.FC<Props> = ({ children }) => {
           <DialogClose asChild>
             <Button variant="destructive" className="w-full sm:w-auto">Cancel</Button>
           </DialogClose>
-          <Button className="w-full sm:w-auto py-6 sm:py-0">Upload</Button>
+          <Button className="w-full sm:w-auto py-6 sm:py-0" onClick={OnUpload}>
+            {loading?
+            <Loader2 className="animate-spin"/>: "Upload"}
+            </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
